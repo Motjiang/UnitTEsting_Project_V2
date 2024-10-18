ad6f83c0403616b01414bd4389ac478c031c3c5f
Motjiang <motjiangbrilliant@gmail.com>
2024-10-18 17:13:06 +0200

    Added a FluentAssertions Package

---
+++ UnitTEsting_Project_V2/Ping/NetworkService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTEsting_Project_V2.Ping
+{
+    public class NetworkService
+    {
+        // string method
+        public string sendPing()
+        {
+            return "Success: Ping sent";
+        }
+
+    }
+}
---
+++ XUnit_Project.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTEsting_Project_V2.Ping;
+
+namespace XUnit_Project.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange
+           //Act
+           var pingService = new NetworkService();
+
+            //Assert
+
+        }
+    }
+}
